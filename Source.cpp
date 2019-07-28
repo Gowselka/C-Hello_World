7d32cafbf528b7dadd71b9d8c73473da1f4206c5
Gowselka <53410914+Gowselka@users.noreply.github.com>
2019-07-28 17:57:48 -0400

    Added Code for project from system

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+// My first C++ program HelloWorld
+// Kevin Gowsell
+// January 13th, 2019
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	system("pause");
+	return 0;
+}
